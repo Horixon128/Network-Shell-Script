45ebb86d7b8346661ad258346045ffbe00f11807
avatarclon <DaviidRodriguez777@gmail.com>
2024-02-16 15:24:14 -0500

    adding traceroute bandwith and nmpa testing to the final code

--- networkScrip.sh
+++ networkScrip.sh
@@ -32,3 +32,26 @@ echo "4. DNS Resolution"
 nslookup www.google.com.co
 
 echo "-----------------------------"
+
+# Check open ports on local Machine
+read -p "Enter the IP Address ir hostname to check open ports on: " remote_hosts
+echo "Checking Open Ports on $remote_host"
+nmap -p- $remote_host
+echo "------------------------------"
+
+# Traceroute to a remote hosts
+read -p " Enter the ip adress or hostname to trace route to: " trace_host
+echo " Trace route to : $trace_host"
+tracert $trace_host
+echo "------------------------------"
+
+# Test Banwidth using Speed test Cli for Windows
+read -p "Do yo want to test nbandwith with speedtest CLI? (y/n) " speedtest_option
+
+if["speedtest_option" == "y"]; then
+./speedtestbyookia_x64
+
+fi
+echo "--------------------------------"
+
+echo " Network testing Completed"
